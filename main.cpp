0b5aa46b33ec38bec88354a0f7caf921621d98e4
andkem <kempe@lysator.liu.se>
2015-02-09 23:31:03 +0100

    Basic skeleton for listener in place.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,59 @@
 #include <iostream>
+#include <string>
+#include <stdexcept>
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+#include <boost/program_options.hpp>
+
+#include "server.h"
+
+namespace po = boost::program_options;
+using namespace std;
+
+void parse_command_line(int argc, char **argv, po::variables_map& vm)
+{
+    po::options_description desc("Command line");
+    desc.add_options()
+    ("help", "Print usage information.")
+    ("lp", po::value<unsigned int>(), "The port on which to listen.")
+    ("ra", po::value<string>(), "Address to redirect to.")
+    ("rp", po::value<unsigned int>(), "Port to redirect to.");
+
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    
+    if (vm.count("help") || !vm.count("lp") || 
+        !vm.count("ra") || !vm.count("rp"))
+    {
+        cout << desc << "\n";
+        throw invalid_argument("Missing parameters!");
+    }
+    
+    server srv();
+}
+
+int main(int argc, char **argv)
+{
+    po::variables_map vm;
+    
+    try
+    {
+        parse_command_line(argc, argv, vm);
+    }
+    catch (const exception& e)
+    {
+        cout << "Error: " << e.what() << "\n";
+        return 1;  
+    }
+    
+    try
+    {
+        boost::asio::io_service io_service;
+        server server(io_service, tcp::endpoint(tcp::v4(), vm["lp"].as<unsigned int>()), vm["ra"].as<string>(), vm["rp"].as<unsigned int>());
+        io_service.run();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
 }
