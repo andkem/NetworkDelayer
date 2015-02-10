431f6c1193d9e35ce2de4b47a11731e0fef0001c
andkem <kempe@lysator.liu.se>
2015-02-10 14:05:23 +0100

    Added some comments to the code.

--- connection.cpp
+++ connection.cpp
@@ -34,11 +34,13 @@ void connection::start(std::string redirect_address, short unsigned int redirect
     {
         if (!error)
         {
+            // Start the proxy -> client data handling.
             incoming_socket.async_read_some(boost::asio::buffer(data_from_inc_client_buff, max_length), [this, self](const boost::system::error_code& error, std::size_t length)
             {
                 rx_tx_data(error, length, data_from_inc_client_buff, incoming_socket, forwarding_socket);
             });
             
+            // Start the proxy -> server data handling.
             forwarding_socket.async_read_some(boost::asio::buffer(data_from_forward_host_buff, max_length), [this, self](const boost::system::error_code& error, std::size_t length)
             {
                 rx_tx_data(error, length, data_from_forward_host_buff, forwarding_socket, incoming_socket);
@@ -58,17 +60,24 @@ void connection::rx_tx_data(const boost::system::error_code& error, std::size_t 
         std::shared_ptr<boost::asio::deadline_timer> sleep_timer(new boost::asio::deadline_timer(io_service));
         sleep_timer->expires_from_now(boost::posix_time::microseconds(delay_time));
         
+        // Copy the data from the buffer to keep it in memory until the send timer expires.
         char* buffer_cpy = copy_data(buffer, length);
+        
+        // Schedule the data for transmission after a delay of delay_time microseconds. This is done asynchronously to make sure
+        // data is continously received while we wait to send the already received data.
         sleep_timer->async_wait([this, self, sleep_timer, buffer_cpy, length, &rx_socket, &tx_socket](const boost::system::error_code& error)
         {
             tx_socket.async_write_some(boost::asio::buffer(buffer_cpy, length), [this, self, buffer_cpy, &rx_socket, &tx_socket](const boost::system::error_code& error, std::size_t length)
             {
+                // Manually clear the buffered data from memory since it has been sent at this point.
                 delete[] buffer_cpy;
+                
                 if (error)
                     cleanup_on_error(error);
             });
         });
         
+        // Read more data asynchronously and make the recursive callback to this function when data has been received.
         rx_socket.async_read_some(boost::asio::buffer(buffer, max_length), [this, self, buffer, &rx_socket, &tx_socket](const boost::system::error_code& error, std::size_t length)
         {
             rx_tx_data(error, length, buffer, rx_socket, tx_socket);
